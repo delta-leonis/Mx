2670d1d702c7e3b7822f0789522a889e70164c12
Rimon Oz <rimooz@gmail.com>
2018-02-19 11:59:18 +0100

    add muxFirst and first tests

---
+++ src/test/java/io/leonis/mx/MxFirst.java
@@ -0,0 +1,23 @@
+package io.leonis.mx;
+
+import org.junit.*;
+
+public class MxFirst {
+  @Test
+  public void muxFirst() throws Exception {
+    Assert.assertEquals(
+        Mx.muxFirst("#", hashbang -> hashbang + "#")
+            .add(marker -> marker + "1")
+            .demux(),
+        "##1");
+  }
+  @Test
+  public void first() throws Exception {
+    Assert.assertEquals(
+        Mx.first(hashbang -> hashbang + "#")
+            .add(marker -> marker + "1")
+            .demux()
+            .apply("#"),
+        "##1");
+  }
+}
